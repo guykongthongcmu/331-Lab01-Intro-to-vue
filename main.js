19cd521681435dcdf4577600032e5bf2272d00d0
guykongthong <guykongthong@gmail.com>
2026-07-06 00:07:48 +0700

    step 3

--- main.js
+++ main.js
@@ -3,10 +3,12 @@ const { createApp, ref } = Vue
 createApp({
     setup(){
         const product = ref('Boots')
-        const productDescription = ref('Footwear for all terain use-cases!')
+        const image = ref('assets/images/socks_green.jpg')
+        const url = ref('https://www.camt.cmu.ac.th/en/home/')
         return {
             product,
-            productDescription
+            image,
+            url
         }
     }
 }).mount('#app')
