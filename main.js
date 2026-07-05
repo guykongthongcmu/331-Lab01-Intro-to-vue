32a80f1d3478f3c8aca084e071e4723842871603
guykongthong <guykongthong@gmail.com>
2026-07-06 00:14:35 +0700

    step 4

--- main.js
+++ main.js
@@ -5,10 +5,15 @@ createApp({
         const product = ref('Boots')
         const image = ref('assets/images/socks_green.jpg')
         const url = ref('https://www.camt.cmu.ac.th/en/home/')
+        const inStock = ref(true)
+        const inventory = ref(100)
+        const onSale = ref(true)
         return {
             product,
             image,
-            url
+            url,
+            inventory,
+            onSale
         }
     }
 }).mount('#app')
