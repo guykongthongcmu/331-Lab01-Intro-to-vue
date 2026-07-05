6ffce763ce23bf0ecb3def1beae1f3bf6b317dce
guykongthong <guykongthong@gmail.com>
2026-07-06 00:36:47 +0700

    step 5

--- main.js
+++ main.js
@@ -8,12 +8,29 @@ createApp({
         const inStock = ref(true)
         const inventory = ref(100)
         const onSale = ref(true)
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const variants = ref([
+            { id: 2234, color: 'green'},
+            { id: 2235, color: 'blue'}
+        ])
+        const sizes = ref([
+            { id: 2236, size: 'S'},
+            { id: 2237, size: "M"},
+            { id: 2238, size: "L"}
+        ])
         return {
             product,
             image,
             url,
             inventory,
-            onSale
+            details,
+            onSale,
+            variants,
+            sizes
         }
     }
 }).mount('#app')
