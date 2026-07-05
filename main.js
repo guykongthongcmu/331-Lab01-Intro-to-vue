6c82c0973b54c7025eb6fc6bebaa18f10aeaf2db
guykongthong <guykongthong@gmail.com>
2026-07-06 01:08:01 +0700

    step 6

--- main.js
+++ main.js
@@ -14,23 +14,41 @@ createApp({
             '20% polyester'
         ])
         const variants = ref([
-            { id: 2234, color: 'green'},
-            { id: 2235, color: 'blue'}
+            { id: 2234, color: 'green', image: 'assets/images/socks_green.jpg'},
+            { id: 2235, color: 'blue', image: 'assets/images/socks_blue.jpg'}
         ])
         const sizes = ref([
             { id: 2236, size: 'S'},
             { id: 2237, size: "M"},
             { id: 2238, size: "L"}
         ])
+        const cart = ref(0)
+
+        function addToCart() {
+            cart.value += 1
+        }
+
+        function updateImage(variantImage) {
+            image.value = variantImage
+        }
+
+        function updateStockStatus() {
+            inStock.value = !inStock.value
+        }
         return {
             product,
             image,
             url,
+            inStock,
             inventory,
             details,
             onSale,
             variants,
-            sizes
+            sizes,
+            cart,
+            addToCart,
+            updateImage,
+            updateStockStatus
         }
     }
 }).mount('#app')
